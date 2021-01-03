b3732b6fea02b03c738af4519e3e553a46a26812
Kat <66186293+katherine-gearhart@users.noreply.github.com>
2021-01-03 11:39:44 -0600

    More refactoring

--- game.hs
+++ game.hs
@@ -4,11 +4,10 @@ main = do putStrLn "Enter your first guess."
           n <- randomRIO (1, 100) :: IO Int
           gameloop n 1
 
-check n g
- | read g < n = "Try higher."
- | read g > n = "Try lower."
+check True = "Try higher."
+check x = "Try lower."
 
 gameloop n x = do g <- getLine
                   if read g /= n
-                  then putStrLn (check n g) >> gameloop n (x + 1)
+                  then putStrLn (check $ read g < n) >> gameloop n (x + 1)
                   else putStrLn $ "You won in " ++ show x ++ " guesses!"
